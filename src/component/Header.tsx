18d3d073597e72c76c00e503af92bb4443651bf3
AromaChai <info.aromafnb@gmail.com>
2026-02-01 18:41:40 +0530

    add

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -43,7 +43,7 @@ const Header = () => {
       </Link>
 
       {/* RIGHT — Order Online */}
-      <Link href="https://aromachai.in/franchise" className="flex flex-col items-center gap-1">
+      <Link href="https://franchise.aromachai.in" className="flex flex-col items-center gap-1">
         <ShoppingBag
           className={`size-5 text-green-700`}
         />
